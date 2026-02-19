b8ce42ba29d8845879482e9d0c2422a2bea2230f
Dai'zjon Carney <89041983+Daizjon@users.noreply.github.com>
2026-02-18 20:10:29 -0600

    Add files via upload

---
+++ Mega2560-C-Interrupts-and-Timer/timer_ctc_mode.c
@@ -0,0 +1,26 @@
+/*
+ * Lab3Activity1.c
+ *
+ * Created: 10/15/2022 9:51:24 AM
+ * Author : dc559
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+int main()
+{
+	DDRB |= (1<<7); //turn portb into output
+	OCR1A = 46872;//3 seconds as(3*16*10^6)/1024
+	TCCR1A = 0x00; //CTC mode, internal clk, prescaler=1024
+	TCCR1B = 0x0D;
+	TIMSK1 = (1<<OCIE1A); //enable Timer1 compare match A int.
+	sei (); //enable interrupt.
+	while(1){}
+}
+ISR (TIMER1_COMPA_vect) //ISR for Timer1 compare match A
+{
+/* Toggle led */
+	PORTB ^= (1 << 7);
+/* Clear counter to restart counting */
+//TCNT1 = 0;
+}
